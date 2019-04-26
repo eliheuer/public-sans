d30a6d23915812053332e202016ac161271b7532
Eli Heuer <eliheuer@gmail.com>
2019-04-25 22:43:17 -0400

    Add build tools
    Build tool list:
        BUILD.sh
        source/BUILD.py

---
+++ BUILD.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python3 sources/BUILD.py \
+#    --ttfautohint "-v --stem-width-mode=qsq" \
+    --googlefonts ~/Google/fonts/ofl/publisans \
+#    --static
+#    --fontbakery \
+#    --drawbot \
+#    --addfont \
+#    --fixnonhinting
